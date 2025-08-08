ee07973cd0f1e361b80b21ca74c744cb768217fe
bnthierr@gmail.com <bnthierr@gmail.com>
2025-08-08 19:41:10 +0000

    second test of deploy to right db

--- models/presentation/second_table.sql
+++ models/presentation/second_table.sql
@@ -1,10 +1,15 @@
 with cte as 
 (select 2 as column_name)  ,
 
+cte3 as 
+(select 200 as column_name)  ,
+
 cte2 as 
 (select 4 as column_name)
 
 select column_name from cte 
 union
-select column_name from cte2
+select column_name from cte2 
+union
+select column_name from cte3
 
